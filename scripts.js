b7c6b97d863b724028f468883a6226c2b73a1750
Zephyr <zhentaop118@gmail.com>
2025-09-01 19:34:58 -0400

    removed template comments

--- scripts.js
+++ scripts.js
@@ -1,4 +1,3 @@
-/*** You will not need this file until Unit 5 ***/
 
 function playMusic(trackName) {
   const audio = document.getElementById("audioPlayer");
@@ -8,25 +7,12 @@ function playMusic(trackName) {
   audio.play()
 }
 
-/*** Dark Mode ***
-
-Purpose:
-- Use this starter code to add a dark mode feature to your website.
-
-When To Modify:
-- [ ] Project 5 (REQUIRED FEATURE) 
-- [ ] Any time after
-***/
-
-// Step 1: Select the theme button
 let themeButton = document.getElementById("theme-button")
 // Step 2: Write the callback function
 const toggleDarkMode = () => {
   document.body.classList.toggle("dark-mode");
 }
 
-// Step 3: Register a 'click' event listener for the theme button,
-//             and tell it to use toggleDarkMode as its callback function
 themeButton.addEventListener("click", toggleDarkMode)
 
 
